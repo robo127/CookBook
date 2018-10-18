c13bccde6e48de3bf6e119cfd0864fdfc335e1bf
robo127 <44208769+robo127@users.noreply.github.com>
2018-10-17 18:03:21 -0700

    Create Recipe.java

---
+++ Recipe.java
@@ -0,0 +1,27 @@
+public class Recipe
+{
+   private String name;
+   private String ingredients;
+   private String instructions;
+   private String information;
+   
+   public Recipe(){
+    name="";
+    ingredients="";
+    insructions="";
+    information="";
+   }
+   
+   public String get_name(){
+    return name;
+   }
+   public String get_ingredients(){
+    return ingredients;
+   }
+   public String get_instructions(){
+    return instruction;
+    }
+   public String get_information(){
+    return information;
+   }
+} 
