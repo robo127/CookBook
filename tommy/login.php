4f83b2ddd567fcfe6a908b3a8a3712e54e8b19b9
tommychavez <44246769+tommychavez@users.noreply.github.com>
2018-11-20 13:36:50 -0800

    Add files via upload

---
+++ tommy/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Page </title>
+	<link rel="stylesheet" href="css/style.css"/> 
+</head>
+<body>
+	<div class ="s004">
+	<div id="frm">
+	<form action="process.php" method = "POST">
+		<p>
+			<label>Username:</label>
+			<input type="text" id="user" name ="user"   />
+		</p>
+		<p>
+			<label>Password:</label>
+			<input type="password" id="pass" name ="pass"   />
+		</p>
+		<p>
+			<input type="submit" id="btn" value ="Login"   /> 
+		</p>
+	
+	</form>
+	</div>
+	</div>
+</body>
+</html>
