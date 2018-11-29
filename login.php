1ceac42bfc4d33021e2ccd8b81dfa1f51373542d
tommychavez <44246769+tommychavez@users.noreply.github.com>
2018-11-29 15:19:06 -0800

    Update login.php

--- login.php
+++ login.php
@@ -5,6 +5,7 @@
 	<link rel="stylesheet" href="css/style.css"/> 
 </head>
 <body>
+	<div class ="s004">
 	<div id="frm">
 	<form action="process.php" method = "POST">
 		<p>
@@ -16,11 +17,16 @@
 			<input type="password" id="pass" name ="pass"   />
 		</p>
 		<p>
-			<input type="submit" id="btn" value ="Login"   />
+		
+			
+		<!<input type="FORM"  value ="Login"   name="save" />			
+		<!<input type="FORM"  value ="Register" name="appprove"  />	
+		<button type="submit" name="camper" formaction="process.php">Login</button>
+		<button type="submit" name="camper" formaction="reg.php">Register</button>
 		</p>
-	
 	</form>
+	
 	</div>
-
+	</div>
 </body>
 </html>
